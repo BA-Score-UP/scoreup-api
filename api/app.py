eed6c81b871cc17555fff475ea25826a7ff07d4a
Augustocfq <augustocfq@gmail.com>
2023-04-24 14:18:25 -0300

    refactor: created a config file

--- api/app.py
+++ api/app.py
@@ -1,17 +1,12 @@
 from flask import Flask
-from flask.json import JSONEncoder
-from bson import ObjectId
+from config import CustomJSONEncoder
 
 from flaskr.routes.test import test_route
 from flaskr.routes.get_question import get_question_route
 
-class CustomJSONEncoder(JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, ObjectId):
-            return str(obj)
-        return super().default(obj)
 
 app = Flask(__name__)
+
 app.json_encoder = CustomJSONEncoder
 
 app.register_blueprint(test_route)
