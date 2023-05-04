4688076f6b4e168f6987bd984a9aebeb8680d540
Augustocfq <augustocfq@gmail.com>
2023-05-04 20:22:58 -0300

    refactor: changed http request methods

--- api/question.py
+++ api/question.py
@@ -3,7 +3,7 @@ from services.question_service import *
 
 question_route: Blueprint = Blueprint('question_route', __name__)
 
-@question_route.route('/question/get_filtered', methods=["GET"])
+@question_route.route('/question/get_filtered', methods=["POST"])
 def get_filtered_questions() -> dict:
     body = request.get_json()
     return get_filtered_questions_service(body)
